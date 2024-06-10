7277fe6c7d187611f8eabe6e276efd70fc87c203
Arne <arnev.c.23@gmail.com>
2024-06-11 00:19:45 +0200

    set up checkpoint system

--- Assets/Scripts/CarAgent.cs
+++ Assets/Scripts/CarAgent.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 public class CarAgent : Agent
 {
     public CarController carController;
+    private bool[] checkpointArray = new bool[29]; //should always be an array of false bools as that is the default value
+    private int currentCheckpointIndex = 0;
 
     public override void Initialize()
     {
@@ -16,6 +19,8 @@ public class CarAgent : Agent
     {
         // Reset the car and environment at the beginning of each episode
         carController.ResetCar();
+        checkpointArray = new bool[29];
+        currentCheckpointIndex = 0;
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -44,5 +49,67 @@ public class CarAgent : Agent
         var continuousActions = actionsOut.ContinuousActions;
         continuousActions[0] = Input.GetAxis("Vertical"); // throttle
         continuousActions[1] = Input.GetAxis("Horizontal"); // steer
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("checkpoint" + currentCheckpointIndex))
+        {
+            checkpointArray[currentCheckpointIndex] = true;
+            if(currentCheckpointIndex != 0)
+            {
+                if (checkpointArray[currentCheckpointIndex - 1] == true) //car has gone trough previous checkpoint
+                {
+                    Debug.Log("checkpoint achieved");
+                    int newIndex = IncrementCurrentCheckpointIndex(currentCheckpointIndex);
+                    currentCheckpointIndex = newIndex;
+                    AddReward(1.0f);                    
+                }
+                else
+                {
+                    Debug.Log("checkpoint achieved in the wrong order");
+                    EndEpisode();
+                }
+            }
+            else
+            {
+                if(DidTheCarCollectAllCheckpoints() == true)
+                {
+                    Debug.Log("car crosses the finish line checkpoint again after having ran the whole course");
+                    AddReward(1.0f);
+                    EndEpisode();
+                }
+                else
+                {
+                    Debug.Log("car crosses finish line to start the race");
+                    int newIndex = IncrementCurrentCheckpointIndex(currentCheckpointIndex);
+                    currentCheckpointIndex = newIndex;
+                    AddReward(1.0f);
+                }
+            }            
+        }
+    }
+    private bool DidTheCarCollectAllCheckpoints()
+    {
+        bool returnvalue = true;
+        foreach(bool checkpointflag in checkpointArray)
+        {
+            if(checkpointflag == false)
+            {
+                returnvalue = false;
+            }
+        }
+        return returnvalue;
+    }
+    private int IncrementCurrentCheckpointIndex(int oldCheckpointIndex)
+    {
+        if(oldCheckpointIndex < 29)
+        {
+            return oldCheckpointIndex++;
+        }
+        else
+        {
+            return 0;
+        }
     }
 }

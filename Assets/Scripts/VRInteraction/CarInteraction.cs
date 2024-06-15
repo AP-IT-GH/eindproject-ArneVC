6c06f5fec5e6c08a4238c24ba7e3479ebc555892
Arne <arnev.c.23@gmail.com>
2024-06-15 14:09:31 +0200

    outline car that's being hovered over

--- Assets/Scripts/VRInteraction/CarInteraction.cs
+++ Assets/Scripts/VRInteraction/CarInteraction.cs
@@ -6,6 +6,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class CarInteraction : XRBaseInteractable
 {
     private GameManager gameManager;
+    private Outline outline;
     protected override void OnEnable()
     {
         base.OnEnable();
@@ -17,6 +18,16 @@ public class CarInteraction : XRBaseInteractable
         if (gameManager == null)
         {
             Debug.LogError("GameManager not found");
+        }
+
+        outline = GetComponent<Outline>();
+        if (outline == null)
+        {
+            Debug.LogError("Outline component not found on the GameObject.");
+        }
+        else
+        {
+            outline.enabled = false;
         }
     }
 
@@ -33,12 +44,20 @@ public class CarInteraction : XRBaseInteractable
         {
             gameManager.CarHover(gameObject);
         }
+        if(outline != null)
+        {
+            outline.enabled = true;
+        }
     }
     private void OnHoverExited(HoverExitEventArgs args)
     {
         if (gameManager != null)
         {
             gameManager.CarHoverExit(gameObject);
+        }
+        if (outline != null)
+        {
+            outline.enabled = false;
         }
     }
 
